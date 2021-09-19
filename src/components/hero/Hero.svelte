6f48e274914c2fecb91b38225fa42caa9ae27057
Hhernanoliva <hhernanoliva@gmail.com>
2021-09-19 20:53:25 -0300

    update

--- src/components/hero/Hero.svelte
+++ src/components/hero/Hero.svelte
@@ -1,18 +1,13 @@
 <script>
- import Cat from "./Cat.svelte"
+ import Cat from "../hero/Cat.svelte"
 </script>
 
-
-
-
 <!-- <HeroCat /> -->
-
-
 <section class="text-gray-600 body-font">
   <div class="container mx-auto flex px-5 md:flex-row flex-col items-center">
     <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
       <div class="cat">
-        <Cat />
+        <Cat/>
       </div>
     </div>
     <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
